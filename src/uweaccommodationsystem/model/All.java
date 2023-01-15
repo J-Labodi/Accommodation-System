1e169bcc65b756bf9bb2a38b648cf2625c5006e1
Jeno Labodi <79979904+J-Labodi@users.noreply.github.com>
2023-01-15 17:19:10 +0000

    Update All.java

--- src/uweaccommodationsystem/model/All.java
+++ src/uweaccommodationsystem/model/All.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package uweaccommodationsystem.model;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author Connor
- */
 public class All extends User {
     
 private ArrayList<Hall> halls = new ArrayList<Hall>();
