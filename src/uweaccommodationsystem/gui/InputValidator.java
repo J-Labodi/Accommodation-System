a5332341f8398c59de2fb90a818d209f39a1f300
Jeno Labodi <79979904+J-Labodi@users.noreply.github.com>
2023-01-15 17:17:41 +0000

    Update InputValidator.java

--- src/uweaccommodationsystem/gui/InputValidator.java
+++ src/uweaccommodationsystem/gui/InputValidator.java
@@ -3,12 +3,6 @@ package uweaccommodationsystem.gui;
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Jeno Labodi
